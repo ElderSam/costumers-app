172722a607da46c95d098febd5a208c983d6221c
ElderSam <eldersamuel98@gmail.com>
2021-04-23 11:34:04 -0300

    Costumers: controller

---
+++ backend/src/app/controllers/CostumerController.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from 'express';
+import { verifyFields } from './utils';
+import Costumer from '../models/Costumer';
+
+class CostumerController {
+    async listAll(req: Request, res: Response) { // list all costumers
+        const list = await Costumer.list();
+
+        return res.send({ listCostumers: list });
+    }
+
+    async listById(req: Request, res: Response) { // list costumers by Id
+
+            let { id } = req.params;
+
+            if(isNaN(id as any)) { // checking if the id is valid
+                return res.status(400).send({ error: 'Invalid Id' });
+            }
+
+        const costumer = await Costumer.list(id);
+
+        return res.send({ costumer });
+    }
+
+    async store(req: Request, res: Response) {
+        const costumer = req.body;
+
+        const requiredFields = [
+            'name',
+            'birth_date',
+            'marital_status',
+            'CPF',
+            'city',
+            'country_state' 
+        ];
+
+        const invalidFields = verifyFields(costumer, requiredFields);
+
+        if(invalidFields.error)
+            return res.status(400).send({ invalidFields }); // bad request
+
+        const costumerExists = await Costumer.findByColumn('CPF', costumer.CPF)
+
+        if(costumerExists) { // implementation of the Singleton design pattern
+            return res.status(409).send({ error: 'Já existe um Cliente com este CPF' }); // status 409: conflict
+        }
+
+        const newCostumer = await Costumer.insert(costumer);
+
+        return res.json(newCostumer);
+    }
+
+    async update(req: Request, res: Response) {
+
+        if(JSON.stringify(req.query) === "{}") // if the Params object is empty
+            return res.status(400).send({ error: 'No params received' })
+
+        const { id } = req.params;
+        const auxCostumer = req.query;
+        // console.log(auxCostumer)
+
+        try{
+            const updatedCostumer = await Costumer.update(id, auxCostumer);
+
+            // console.log('UPDATE: ', updatedCostumer);
+
+            return res.json({ message: 'Cliente Atualizado!' });
+
+        }catch(err) {
+            /*const { name, message } = err;
+            console.log({ name, message })*/
+
+            return res.status(400).send({ error: err.message });
+        }
+    }
+
+    async delete(req: Request, res: Response) { // delete Costumer
+        const { id } = req.params;
+
+        const deleteRes = await Costumer.delete(id)
+        // console.log(deleteRes)
+
+        return res.send({ deleted: Boolean(deleteRes.affected), id })
+    }
+}
+
+export default new CostumerController();
