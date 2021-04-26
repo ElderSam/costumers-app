d1875c2d0f4a7851984643fc63e81ec945538985
ElderSam <eldersamuel98@gmail.com>
2021-04-26 19:08:58 -0300

    Backend: enable Cors for external requests

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import cors from 'cors';
+
 import costumerRoutes from './Costumers';
 
 const app = Router();
 
+app.use(cors())
 app.use('/costumers', costumerRoutes);
 
 export default app;
